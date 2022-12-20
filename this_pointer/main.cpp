dbb818c403f274ad40ed5aa9199def0f5d68c909
kongfanyin <103018828+kongfanyin@users.noreply.github.com>
2022-12-20 15:01:36 +0800

    构造函数初始化类对象

--- this_pointer/main.cpp
+++ this_pointer/main.cpp
@@ -9,7 +9,7 @@ const int STKS = 4;
 int main(char argc, char** argv)
 {
 	//create an arry of initialized objects
-
+	//参数列表初始化C++11
 	Stock stocks[STKS]=
 	{
 	{"Nano smart",12,20.0},
@@ -17,6 +17,14 @@ int main(char argc, char** argv)
 	{"monoli obelisk",50,28.8},
 	{"Fleep enterprises",268,483}
 	};
+	//构造函数初始化
+	/*Stock stocks[STKS] =
+	{
+	Stock("Nano smart",12,20.0),
+	Stock("boffo objects",200,40.0),
+	Stock("monoli obelisk",50,28.8),
+	Stock("Fleep enterprises",268,483)
+	};*/
 	std::cout << "Stock holding: \n";
 	for (int st = 0; st < STKS; st++)
 	{
